09ad1951729e682952043c46f93f14729ad41623
tjrwldnjs <43231922+tjrwldnjs@users.noreply.github.com>
2019-12-10 19:55:04 +0900

    Update 4bit.v

--- practice02/4bit.v
+++ practice02/4bit.v
@@ -1,7 +1,7 @@
 module fa4_inst(s,co,a,b,ci);
 
 output  [3:0]		s;
-output				co;
+output			co;
 input   [3:0]		a;
 input   [3:0]	  	b;
 input		        ci;
@@ -11,35 +11,35 @@ wire    [2:0]	 	carry;
 fa_dataflow	   fa_u0(	
 				.s		(	s[0]     	),
 				.co		(	carry[0] 	),
-				.a      (	a[0]     	),
-				.b      (	b[0]     	),
-				.ci     (	ci       	));
+				.a      	(	a[0]     	),
+				.b      	(	b[0]     	),
+				.ci    		(	ci       	));
               
               
               
 
 fa_dataflow	   fa_u1(	
-				.s	    (	s[1]     	),
-				.co     (	carry[1] 	),
-				.a      (	a[1]     	),
-				.b      (	b[1]     	),
-				.ci     (	carry[0]	));
+				.s	    	(	s[1]     	),
+				.co     	(	carry[1] 	),
+				.a      	(	a[1]     	),
+				.b      	(	b[1]     	),
+				.ci     	(	carry[0]	));
               
               
 fa_dataflow 	  fa_u2(	
-				.s	    (	s[2]     	),
-				.co     (	carry[2] 	),
-				.a      (	a[2]     	),
-				.b      (	b[2]     	),
-				.ci     (	carry[1]	));              
+				.s	    	(	s[2]     	),
+				.co     	(	carry[2] 	),
+				.a      	(	a[2]     	),
+				.b      	(	b[2]     	),
+				.ci     	(	carry[1]	));              
               
               
 fa_dataflow   	fa_u3(	
-				.s	    (	s[3]     	),
-				.co     (	co       	),
-				.a      (	a[3]     	),
-				.b      (	b[3]     	),
-				.ci     (	carry[2]	));       
+				.s	    	(	s[3]     	),
+				.co     	(	co       	),
+				.a      	(	a[3]     	),
+				.b      	(	b[3]     	),
+				.ci     	(	carry[2]	));       
               
 endmodule
 
@@ -50,7 +50,7 @@ output  [3:0]		s;
 output		        co;
 input   [3:0]		a;
 input   [3:0]		b;
-input				ci;
+input			ci;
 
 assign    {co, s} = a+b+ci;
 
